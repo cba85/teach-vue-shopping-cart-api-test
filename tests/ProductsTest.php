676db08d9d82642607f52c518be0ef1ffda928d5
Clement Barbaza <clement.barbaza@gmail.com>
2020-05-18 12:23:58 +0200

    add structure tests for products (thx Samuel)

--- tests/ProductsTest.php
+++ tests/ProductsTest.php
@@ -5,7 +5,6 @@ use GuzzleHttp\Client as GuzzleHttp;
 
 final class ProductsTest extends TestCase
 {
-
     public function testProducts()
     {
         global $argv;
@@ -13,9 +12,18 @@ final class ProductsTest extends TestCase
         $client = new GuzzleHttp([
             'base_uri' => $argv[2]
         ]);
+
         $response = $client->get('/api/products');
-        $this->assertEquals(200, $response->getStatusCode());
         $products = json_decode($response->getBody());
+
+        $this->assertEquals(200, $response->getStatusCode());
         $this->assertIsArray($products);
+        $this->assertNotEmpty($products);
+        $this->assertObjectHasAttribute('id', $products[0]);
+        $this->assertObjectHasAttribute('created_at', $products[0]);
+        $this->assertObjectHasAttribute('updated_at', $products[0]);
+        $this->assertObjectHasAttribute('name', $products[0]);
+        $this->assertObjectHasAttribute('price', $products[0]);
+        $this->assertObjectHasAttribute('description', $products[0]);
     }
 }
